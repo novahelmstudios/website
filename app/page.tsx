094adbd8d73e9c6b12339a48553e08fc4592a186
abdulahshoaib <khan.abdullah.shoaib@gmail.com>
2025-06-07 20:26:57 +0500

    feat: navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,9 @@
-"use client"
-import { useState } from "react"
-import Gsap from "@/components/gasp"
-
 export default function Home() {
-    const [gsap, setgsap] = useState<boolean>(true)
     return (
         <>
-            <button onClick={() => setgsap(!gsap)}> play/pause</button>
-            {(!gsap) ?
-                <Gsap />
-                :
-                <h1>hi ah*s*an</h1>
-            }
+            <nav>
+                Novahelm Studios
+            </nav>
         </>
     );
 }
